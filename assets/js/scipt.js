d55a9d4c3f743f786c2c8e450f6ca64db4e45013
Daniel Packer <danielapacker@gmail.com>
2022-11-08 21:26:36 -0700

    add search section to html and starter variables to js

---
+++ assets/js/scipt.js
@@ -0,0 +1,4 @@
+const day = document.querySelector(".day");
+const fiveDay = document.querySelector(".fiveDay");
+const city = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
